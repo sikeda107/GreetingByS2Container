1c97eef15faaae72f4132128db258e0dc97a105c
sikeda107 <s.ikeda107@gmail.com>
2020-08-02 17:02:01 +0900

    add: あいさつクライアントクラスの実装

--- src/main/java/example/di/impl/GreetingClientImpl.java
+++ src/main/java/example/di/impl/GreetingClientImpl.java
@@ -1,4 +1,18 @@
 package example.di.impl;
 
-public class GreetingClientImpl {
+import example.di.Greeting;
+import example.di.GreetingClient;
+
+public class GreetingClientImpl implements GreetingClient {
+    private Greeting greeting;
+
+    @Override
+    public void setGreeting(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println(greeting.greet());
+    }
 }
